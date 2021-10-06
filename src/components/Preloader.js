5678d57e44ffdf5ef4541b47357bd7f9e139a132
XxTazuxX <anastazim2@gmail.com>
2021-10-07 04:43:43 +0600

    Simple Coming Soon Screen Added

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -2,17 +2,14 @@ function Preloader() {
   return (
     <div className="preloader">
       <div className="loading-text">
-        <span className="loading-text-words" data-preloader="L">
-          L
+        <span className="loading-text-words" data-preloader="C">
+          C
         </span>
         <span className="loading-text-words" data-preloader="O">
           O
         </span>
-        <span className="loading-text-words" data-preloader="A">
-          A
-        </span>
-        <span className="loading-text-words" data-preloader="D">
-          D
+        <span className="loading-text-words" data-preloader="M">
+          M
         </span>
         <span className="loading-text-words" data-preloader="I">
           I
@@ -22,6 +19,21 @@ function Preloader() {
         </span>
         <span className="loading-text-words" data-preloader="G">
           G
+        </span>
+        <span className="loading-text-words" data-preloader="">
+          &nbsp;&nbsp;
+        </span>
+        <span className="loading-text-words" data-preloader="S">
+          S
+        </span>
+        <span className="loading-text-words" data-preloader="O">
+          O
+        </span>
+        <span className="loading-text-words" data-preloader="O">
+          O
+        </span>
+        <span className="loading-text-words" data-preloader="N">
+          N
         </span>
       </div>
     </div>
